4e3533a57aefeba3a8ef5661a4265c995b2bb4e7
jo0132 <noji70770@gmail.com>
2023-05-10 11:45:42 +0900

    2023.05.10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function Hello(props){
+    return <h1>hello, {props.name}</h1> // element안에 있는 속성을 객체로 받아 출력이 가능하다.
+}                               
+const element = <Hello name="arara" />       
 
-export default App;
+ReactDOM.render(element, document.getElementById("root"));
+
+export default Hello
+
+// 전체 셋팅은 rfce
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Hello(props){
+return(
+  <div>
+  <div>{name.name}</div>
+  <div>{name.text}</div>
+  <div>{name.author.name}</div>
+  <div>{name.author.url}</div>
+</div>
+
+)
+
+}
+
+const name = {
+  name : "noji",
+  text : "hello",
+  author : {
+    name : "baby",
+    url : "naver.com"
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Hello title1={name.name} text={name.text} name={name.author.name} url={name.author.url} />)
